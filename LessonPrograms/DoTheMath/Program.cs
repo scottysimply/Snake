d76c7fa97da0dc13f002f0008c2298ce215d193f
scottsimply <scottistriggered@gmail.com>
2023-10-23 09:05:14 -0500

    Initial Commit

---
+++ LessonPrograms/DoTheMath/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace DoTheMath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Assignments
+            byte a = 25;
+            short b = 32000;
+            int c = 1000000;
+            byte x = 5;
+            byte y = 10;
+            byte z = 2;
+            float myFloat = 1.5f;
+
+            // I don't know if I actually saved any time here... it was just easier to write this instead of including a semicolon and newline.
+            Print(x + y, myFloat + myFloat, myFloat + a, c - b, x * a, y * b, c / b, a / x, a % x, c % y);
+
+        }
+        // DRY
+        static void Print(params object[] input)
+        {
+            foreach (object obj in input)
+            {
+                Console.WriteLine(obj);
+            }
+        }
+    }
+}
---
+++ LessonPrograms/OddOrEven/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace OddOrEven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Initializations
+            string input;
+            bool condition = true;
+            int value;
+
+            Console.WriteLine("Enter a number: ");
+
+            // Input validation
+            do
+            {
+                input = Console.ReadLine();
+                // If number could be parsed, exit loop.
+                if (Int32.TryParse(input, out value))
+                {
+                    condition = false;
+                }
+                else
+                {das
+                    Console.WriteLine("No number detected. Try again.");
+                }
+
+            } while (condition);
+
+            // Logic
+            if (value % 2 == 0)
+            {
+                Console.WriteLine($"{value} is even.");
+            }
+            else
+            {
+                Console.WriteLine($"{value} is odd.");
+            }
+        }
+    }
+}
