8818f9dd396092e1726dc592fa3db62bd2f94756
scottsimply <scottistriggered@gmail.com>
2023-11-16 09:50:23 -0600

    Input handler class reads keyboard input

--- Snake/Snake/InputHandler.cs
+++ Snake/Snake/InputHandler.cs
@@ -1,13 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Xna.Framework.Input;
 
 namespace Snake
 {
-    internal class InputHandler
+    public class InputHandler
     {
+        /// <summary>
+        /// The current keyboard state
+        /// </summary>
+        private KeyboardState _currentState;
+
+        /// <summary>
+        /// The old keyboard state
+        /// </summary>
+        private KeyboardState _oldState;
+
+        /// <summary>
+        /// Gets whether the specified <see cref="Keys"/> is pressed.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns>Returns true <i>on the tick the key was pressed</i>.</returns>
+        public bool IsKeyPressed(Keys keyToCheck)
+        {
+            if (_currentState.IsKeyDown(keyToCheck) && !_oldState.IsKeyDown(keyToCheck))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Gets whether the specified <see cref="Keys"/> is held down.
+        /// </summary>
+        /// <param name="keyToCheck"></param>
+        /// <returns>Returns true on <i>every tick</i> the key is held down.</returns>
+        public bool IsKeyHeld(Keys keyToCheck)
+        {
+            if (_currentState.IsKeyDown(keyToCheck))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Reads the current state of the <see cref="Keyboard"/>.
+        /// </summary>
+        public void QueryInput()
+        {
+            _oldState = _currentState;
+            _currentState = Keyboard.GetState();
+        }
 
     }
 }
