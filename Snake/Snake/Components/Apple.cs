85d0b2ac6053f349664be5b4e15f1d388fe02498
scottsimply <scottistriggered@gmail.com>
2023-11-28 08:37:00 -0600

    trivial changes

--- Snake/Snake/Components/Apple.cs
+++ Snake/Snake/Components/Apple.cs
@@ -6,6 +6,6 @@ namespace Snake.Components
 {
     public class Apple
     {
-        
+
     }
 }
--- Snake/Snake/Components/Player.cs
+++ Snake/Snake/Components/Player.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Xna.Framework;
 
 namespace Snake.Components
 {
     public class Player
     {
-        
+        public 
     }
 }
