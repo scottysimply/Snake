a4278f875c17b066fd33c5db73b5ca183dd9ad0a
scottsimply <scottistriggered@gmail.com>
2023-11-28 10:13:47 -0600

    Created apple generator

---
+++ Snake/AppleGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Snake.Components;
+
+namespace Snake
+{
+    public class AppleGenerator
+    {
+        public List<Point> EmptyPoints { get; set; }
+        private Random _rand;
+        public AppleGenerator(ref Grid grid)
+        {
+
+        }
+    }
+}
