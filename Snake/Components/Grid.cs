b637c284f907418c8c97833c5d4d007c7f178f69
scottsimply <scottistriggered@gmail.com>
2023-11-28 09:49:40 -0600

    More grid debugging

--- Snake/Components/Grid.cs
+++ Snake/Components/Grid.cs
@@ -8,11 +8,11 @@ namespace Snake.Components
         public Rectangle Dimensions;
         const int MARGIN = 2;
         public Cell[,] CellArray { get; set; }
-        public Grid(int num_cols, int num_rows, Point parent_dimensions)
+        public Grid(int num_cols, int num_rows, Point parent_size)
         {
             // Parameters for auto-fitting the cell size.
-            int width = num_cols / parent_dimensions.X;
-            int height = num_rows / parent_dimensions.Y;
+            int width = parent_size.X / num_cols;
+            int height = parent_size.Y / num_rows;
 
             // Taking the smaller measurement. Since I'm using a funky operator, I give a tl;dr of what it does below.
             int cell_size = width <= height ? width : height; // This is the ternary operator. It allows you to do boolean assignment.
@@ -28,8 +28,8 @@ namespace Snake.Components
             {
                 for (int x = 0; x < num_cols; x++)
                 {
-                    Rectangle cell_dimensions = new(Dimensions.X + cell_size * num_cols,
-                                                    Dimensions.Y + cell_size * num_rows,
+                    Rectangle cell_dimensions = new(Dimensions.X + cell_size * x,
+                                                    Dimensions.Y + cell_size * y,
                                                     cell_size,
                                                     cell_size);
                     CellArray[x, y] = new(cell_dimensions, new Point(x, y));
