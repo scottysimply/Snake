15a86f84cf0dd8cb3ef4615d834ef8f7c516a886
scottsimply <scottistriggered@gmail.com>
2023-11-29 18:50:36 -0600

    Added player's tracker

--- Snake/Components/Cell.cs
+++ Snake/Components/Cell.cs
@@ -10,9 +10,14 @@ namespace Snake.Components
         /// The ID of the cell. See <see cref="LogicIDs"/>.
         /// </summary>
         public LogicIDs ID { get; set; }
+        /// <summary>
+        /// This is used only if a snake is in the cell. 0 represents the head, and will count up until the very end of the tail. -1 means no player is in the cell.
+        /// </summary>
+        public int WhoAmI { get; set; }
         public Point Coordinates { get; set; }
         public Cell(Rectangle dimensions, Point location)
         {
+            WhoAmI = -1;
             Dimensions = dimensions;
             Coordinates = location;
             ResetCell();
