e6f9dc3a070ad881e391c20dcbdb5bbac7f36528
scottsimply <53203532+scottysimply@users.noreply.github.com>
2023-12-03 19:43:36 -0600

    Renamed collection of files

---
+++ Snake/AssetList.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+
+namespace Snake
+{
+    public static class AssetList
+    {
+        public static Texture2D TBlankCell { get; set; }
+        public static Texture2D TSnake { get; set; }
+        public static Texture2D TApple { get; set; }
+        public static Texture2D BlankSquare { get; set; }
+        public static SpriteFont ArialLarge { get; set; }
+        public static SpriteFont ArialSmall { get; set; }
+    }
+}
