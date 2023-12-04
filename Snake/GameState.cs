d791c5b2fbcd16e57f5c9354425b98e0d62fbda6
scottsimply <53203532+scottysimply@users.noreply.github.com>
2023-12-03 19:43:01 -0600

    Added gamestate to keep track of the game

---
+++ Snake/GameState.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    public enum GameState
+    {
+        Inactive, // In this state, the user can press any key and the game will begin.
+        Playing, // In this state, the user is actively playing the game
+    }
+}
