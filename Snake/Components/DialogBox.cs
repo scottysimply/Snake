92d5fda1d62dc0dbc9897bf9d98d848f8283f473
scottsimply <53203532+scottysimply@users.noreply.github.com>
2023-12-03 19:43:14 -0600

    Created dialog box object

---
+++ Snake/Components/DialogBox.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Snake.Components
+{
+    public class DialogBox
+    {
+        public Rectangle Border;
+        public string Text;
+        public DialogBox(Rectangle dimensions, string text)
+        {
+            Text = text;
+            Border = dimensions;
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(AssetList.BlankSquare, Border, Color.MediumSlateBlue);
+            Vector2 text_offset = AssetList.ArialLarge.MeasureString(Text);
+            spriteBatch.DrawString(AssetList.ArialLarge, Text, Border.Center.ToVector2() - text_offset / 2, Color.WhiteSmoke);
+        }
+    }
+}
